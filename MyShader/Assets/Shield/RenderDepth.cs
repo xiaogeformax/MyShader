03dc7e118762d0eabba572d8d0edfb680de5b5b5
xiaogeformax <554378242@qq.com>
2017-09-12 20:51:55 +0800

    护盾能量罩 shader

---
+++ MyShader/Assets/Shield/RenderDepth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderDepth : MonoBehaviour {
+
+	void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+}

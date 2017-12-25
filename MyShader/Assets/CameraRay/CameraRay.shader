7d63f903fe7f19333d7afefce7842de57ad86d67
xiaogeformax <554378242@qq.com>
2017-12-25 15:23:38 +0800

    Camera ray make object transparent,so target will  always  been see

---
+++ MyShader/Assets/CameraRay/CameraRay.shader
@@ -0,0 +1,37 @@
+﻿//增加prepass的半透渲染  
+//by：puppet_master  
+//2017.7.23  
+Shader "ApcShader/OcclusionTransparent" {  
+    Properties {  
+        _Color ("Main Color", Color) = (1,1,1,1)  
+        _MainTex ("Base (RGB)", 2D) = "white" {}  
+    }  
+    SubShader {  
+        Tags { "RenderType"="Transparent" "Queue" = "Transparent-1"}  
+          
+        Pass  
+        {  
+            ZWrite On   //开启深度写入  
+            ColorMask 0 //不写颜色  
+        }     
+          
+        ZWrite Off  
+        CGPROGRAM  
+        #pragma surface surf Lambert alpha  
+  
+        sampler2D _MainTex;  
+        fixed4 _Color;  
+  
+        struct Input {  
+            float2 uv_MainTex;  
+        };  
+  
+        void surf (Input IN, inout SurfaceOutput o) {  
+            fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;  
+            o.Albedo = c.rgb;  
+            o.Alpha = c.a;  
+        }  
+        ENDCG  
+    }   
+    FallBack "VertexLit"  
+}
---
+++ MyShader/Assets/CameraRay/XrayCameral.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XrayCameral : MonoBehaviour
+{
+
+    public Transform target;
+    public List<Renderer> listLastRender = new List<Renderer>();
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        //first,set all hit render value opacity
+        for (int i = 0; i < listLastRender.Count; i++)
+        {
+            TransparencySet(listLastRender[i], 1.0f);
+        }
+        Vector3 tarDir = (target.position - transform.position).normalized;
+        Debug.DrawLine(target.position, transform.position, Color.red);
+
+        float targetDis = Vector3.Distance(target.position, transform.position);
+        RaycastHit[] listHitObj = Physics.RaycastAll(transform.position, tarDir, targetDis);
+        Debug.Log(listHitObj.Length);
+        for (int i = 0; i < listHitObj.Length; i++)
+        {
+            RaycastHit hit = listHitObj[i];
+            if (hit.transform == target.transform)
+            {
+                continue;
+            }
+            Renderer renderer = hit.collider.GetComponent<Renderer>();
+            listLastRender.Clear();
+
+            if (renderer)
+            {
+                listLastRender.Add(renderer);
+               
+                TransparencySet(renderer, 0.1f);
+            }
+            // 使用 render 去使 MeshRender fasle,使其完全透明
+//             foreach (var lastRend in listLastRend)
+//             {
+//                 lastRend.enabled = false;
+//             }
+        }
+
+    }
+
+    void TransparencySet(Renderer renderer, float a)
+    {
+        renderer.material.shader = Shader.Find("ApcShader/OcclusionTransparent");
+        renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, a);
+    }
+}
